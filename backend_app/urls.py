881a00e2fcb9ddfa09d1ce77783d4d5519dd5982
SRIDHAR-08 <sridhar200288@gmail.com>
2024-03-21 17:52:03 +0530

    3

--- backend_app/urls.py
+++ backend_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('home/',home_page),
+    path('',home_page),
     path('form/',form_page),
     path('view/',view_page),
     path('delect/<int:id>',delete_student,name="delect_student"),
